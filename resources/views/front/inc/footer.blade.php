d53a9ce0b3914c4f4413bb90e966b3843d2984be
Adi1162 <nursholihinadip@gmail.com>
2024-01-28 17:28:53 +0700

    final project

---
+++ resources/views/front/inc/footer.blade.php
@@ -0,0 +1,25 @@
+<footer id="footer">
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <h3>Kurnia Mardani Tour</h3>
+            <p>
+              Langslebar Laok, Bira Timur.<br>
+              Kec. Sokobanah, Kabupaten Sampang, Jawa Tengah 69262<br>
+              Indonesia <br><br>
+              <strong>Phone:</strong> +62 8522 6966 286<br>
+              <strong>Email:</strong> kurniamarnaditour@gmail.com<br>
+            </p>
+        <div class="copyright">
+          &copy; Copyright <strong><span>Kurnia Mardani Tour</span></strong>. All Rights Reserved By : Nur Sholihin Adi Pradana
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/arfa_travel003?igsh=OWI3MGdhMTFjaDd2" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer>
